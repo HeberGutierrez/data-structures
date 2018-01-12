783a1cacb9217642311af52ce74e60de947f57aa
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-01-12 08:08:23 -0600

    set - solved

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  let index = this._storage.indexOf(item);
+  this._storage.splice(index, 1);
 };
-
 /*
  * Complexity: What is the time complexity of the above functions?
+ * .add
+ * .remove
  */
